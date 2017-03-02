8e5857f8bcc76eac146167b021c6faa930197fa4
Eric Pauley <eric@pauley.me>
2017-03-02 02:55:30 -0500

    Added aggregation settings and optimization

--- mediancut.go
+++ mediancut.go
@@ -6,6 +6,15 @@ import (
 	"sort"
 )
 
+type colorAxis int
+
+// Color axis constants
+const (
+	RED colorAxis = iota
+	GREEN
+	BLUE
+)
+
 type colorPriority struct {
 	c color.RGBA
 	p uint64
@@ -16,11 +25,25 @@ type colorBucket struct {
 	min      color.RGBA
 	max      color.RGBA
 	priority uint64
-	passed   bool
 }
 
+// AggregationType specifies the type of aggregation to be done
+type AggregationType int
+
+const (
+	// MODE - pick the highest priority value
+	MODE AggregationType = iota
+	// MEAN - weighted average all values
+	MEAN
+)
+
 // MedianCutQuantizer implements the go draw.Quantizer interface using the Median Cut method
-type MedianCutQuantizer struct{}
+type MedianCutQuantizer struct {
+	// The type of aggregation to be used to find final colors
+	Aggregation AggregationType
+	// The weighting function to use on each pixel
+	Weighting func(image.Image, int, int) uint64
+}
 
 func colorSpan(colors []colorPriority) (min color.RGBA, max color.RGBA, priority uint64) {
 	min = color.RGBA{255, 255, 255, 255}
@@ -28,6 +51,9 @@ func colorSpan(colors []colorPriority) (min color.RGBA, max color.RGBA, priority
 	for _, c := range colors {
 		priority += c.p
 		r, g, b, _ := c.c.RGBA()
+		r /= 257
+		g /= 257
+		b /= 257
 		if uint8(r) < min.R {
 			min.R = uint8(r)
 		}
@@ -50,69 +76,132 @@ func colorSpan(colors []colorPriority) (min color.RGBA, max color.RGBA, priority
 	return
 }
 
-func (q MedianCutQuantizer) quantizeSlice(p color.Palette, colors []colorPriority) color.Palette {
+func bucketize(colors []colorPriority, num int) (buckets []colorBucket) {
 	var bucket colorBucket
 	bucket.colors = colors
 	bucket.min, bucket.max, bucket.priority = colorSpan(bucket.colors)
-	var buckets []colorBucket
 	buckets = append(buckets, bucket)
-	for len(buckets) < cap(p) && !buckets[0].passed {
+	for len(buckets) < num && len(buckets) < len(colors) {
 		bucket, buckets = buckets[0], buckets[1:]
-		if len(bucket.colors) <= 1 { // Mark a bucket and requeue if it only has one element
-			bucket.passed = true
+		if len(bucket.colors) < 2 {
 			buckets = append(buckets, bucket)
 			continue
 		}
 		rspan := bucket.max.R - bucket.min.R
 		gspan := bucket.max.G - bucket.min.G
 		bspan := bucket.max.B - bucket.min.B
-		b1, b2 := bucket, bucket
-		if rspan > gspan && rspan > bspan { // Red is greatest span
-			sort.Slice(bucket.colors, func(i, j int) bool {
-				return bucket.colors[i].c.R < bucket.colors[j].c.R
-			})
-			b1.max.R = bucket.colors[len(bucket.colors)/2-1].c.R
-			b2.min.R = bucket.colors[len(bucket.colors)/2].c.R
-		} else if gspan > bspan { // Green is greatest span
-			sort.Slice(bucket.colors, func(i, j int) bool {
-				return bucket.colors[i].c.G < bucket.colors[j].c.G
-			})
-			b1.max.G = bucket.colors[len(bucket.colors)/2-1].c.G
-			b2.min.G = bucket.colors[len(bucket.colors)/2].c.G
-		} else { // Blue is greatest span
-			sort.Slice(bucket.colors, func(i, j int) bool {
-				return bucket.colors[i].c.B < bucket.colors[j].c.B
-			})
-			b1.max.B = bucket.colors[len(bucket.colors)/2-1].c.B
-			b2.min.B = bucket.colors[len(bucket.colors)/2].c.B
+		var span colorAxis
+
+		if rspan > gspan && rspan > bspan {
+			span = RED
+		} else if gspan > bspan {
+			span = GREEN
+		} else {
+			span = BLUE
 		}
 
-		b1.colors = bucket.colors[:len(bucket.colors)/2]
-		b2.colors = bucket.colors[len(bucket.colors)/2:]
-
-		buckets = append(buckets, b1, b2)
-	}
-	for _, bucket := range buckets {
-		var best *colorPriority
-		for _, c := range bucket.colors {
-			if best == nil || c.p > best.p {
-				best = &c
+		sort.Slice(bucket.colors, func(i, j int) bool {
+			r1, g1, b1, _ := bucket.colors[i].c.RGBA()
+			r2, g2, b2, _ := bucket.colors[i].c.RGBA()
+			switch span {
+			case RED:
+				return r1 < r2
+			case GREEN:
+				return g1 < g2
+			default:
+				return b1 < b2
 			}
+		})
+
+		bucket1, bucket2 := bucket, bucket
+
+		var p uint64
+		var i int
+
+		for ; i < len(bucket.colors)-1 && p < bucket.priority; i++ {
+			p += bucket.colors[i].p
 		}
-		p = append(p, best.c)
+
+		bucket1.priority = p
+		bucket2.priority = bucket.priority - p
+
+		r1, g1, b1, _ := bucket.colors[i-1].c.RGBA()
+		r2, g2, b2, _ := bucket.colors[i].c.RGBA()
+
+		switch span {
+		case RED:
+			bucket1.max.R = uint8(r1 / 257)
+			bucket2.min.R = uint8(r2 / 257)
+		case GREEN:
+			bucket1.max.G = uint8(g1 / 257)
+			bucket2.min.G = uint8(g2 / 257)
+		case BLUE:
+			bucket1.max.B = uint8(b1 / 257)
+			bucket2.min.B = uint8(b2 / 257)
+		}
+
+		bucket1.colors = bucket.colors[:i]
+		bucket2.colors = bucket.colors[i:]
+
+		buckets = append(buckets, bucket1, bucket2)
+	}
+	return
+}
+
+func (q MedianCutQuantizer) palettize(p color.Palette, buckets []colorBucket) color.Palette {
+	for _, bucket := range buckets {
+		switch q.Aggregation {
+		case MEAN:
+			var r, g, b uint64
+			for _, c := range bucket.colors {
+				cr, cg, cb, _ := c.c.RGBA()
+				r += uint64(cr) * c.p
+				g += uint64(cg) * c.p
+				b += uint64(cb) * c.p
+			}
+			r /= uint64(len(bucket.colors)) * bucket.priority
+			g /= uint64(len(bucket.colors)) * bucket.priority
+			b /= uint64(len(bucket.colors)) * bucket.priority
+			p = append(p, color.RGBA{uint8(r), uint8(g), uint8(b), 255})
+		case MODE:
+			var best *colorPriority
+			for _, c := range bucket.colors {
+				if best == nil || c.p > best.p {
+					best = &c
+				}
+			}
+			p = append(p, best.c)
+		}
 	}
 	return p
 }
 
-func buildBucket(m image.Image) []colorPriority {
+func (q MedianCutQuantizer) quantizeSlice(p color.Palette, colors []colorPriority) color.Palette {
+	buckets := bucketize(colors, cap(p)-len(p))
+	return q.palettize(p, buckets)
+}
+
+func (q MedianCutQuantizer) buildBucket(m image.Image) []colorPriority {
 	colors := make(map[color.Color]uint64)
-	for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
-		for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
-			colors[m.At(x, y)]++
+	if q.Weighting != nil {
+		for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
+			for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
+				colors[m.At(x, y)] += q.Weighting(m, x, y)
+			}
+		}
+	} else {
+		for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
+			for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
+				colors[m.At(x, y)]++
+			}
 		}
 	}
+
 	var colorSlice []colorPriority
 	for c, priority := range colors {
+		if priority == 0 {
+			continue
+		}
 		r, g, b, _ := c.RGBA()
 		c := color.RGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), 255}
 		colorSlice = append(colorSlice, colorPriority{c, priority})
@@ -122,5 +211,5 @@ func buildBucket(m image.Image) []colorPriority {
 
 // Quantize quantizes an image to a palette
 func (q MedianCutQuantizer) Quantize(p color.Palette, m image.Image) color.Palette {
-	return q.quantizeSlice(p, buildBucket(m))
+	return q.quantizeSlice(p, q.buildBucket(m))
 }
--- mediancut_test.go
+++ mediancut_test.go
@@ -3,6 +3,7 @@ package quantize
 import (
 	"bufio"
 	"image"
+	"image/color"
 	"image/gif"
 	"os"
 	"testing"
@@ -19,7 +20,20 @@ func TestBuildBucket(t *testing.T) {
 	if err != nil {
 		t.Fatal("Couldn't decode test file")
 	}
-	colors := buildBucket(i)
+
+	q := MedianCutQuantizer{MODE, nil}
+
+	colors := q.buildBucket(i)
+	t.Logf("Naive color map contains %d elements", len(colors))
+
+	q = MedianCutQuantizer{MODE, func(i image.Image, x int, y int) uint64 {
+		if x < 2 || y < 2 || x > i.Bounds().Max.X-2 || y > i.Bounds().Max.X-2 {
+			return 1
+		}
+		return 0
+	}}
+
+	colors = q.buildBucket(i)
 	t.Logf("Color map contains %d elements", len(colors))
 }
 
@@ -32,8 +46,8 @@ func TestQuantize(t *testing.T) {
 	if err != nil {
 		t.Fatal("Couldn't decode test file")
 	}
-	q := MedianCutQuantizer{}
-	p := q.Quantize(nil, i)
+	q := MedianCutQuantizer{MODE, nil}
+	p := q.Quantize(make([]color.Color, 0, 256), i)
 	t.Logf("Created palette with %d colors", len(p))
 
 	f, err := os.Create("test_output.gif")
@@ -41,7 +55,7 @@ func TestQuantize(t *testing.T) {
 		t.Fatal("Couldn't open output file")
 	}
 
-	options := gif.Options{NumColors: 16, Quantizer: q, Drawer: nil}
+	options := gif.Options{NumColors: 256, Quantizer: q, Drawer: nil}
 
 	w := bufio.NewWriter(f)
 
